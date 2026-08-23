ecbd2c8db378cefe5631a84202fc08a881fe4af7
382868331 <382868331@users.noreply.github.com>
2026-08-24 03:16:24 +0800

    challenge: reproduce 仅零值默认策略条件反转

---
+++ task_018_test.go
@@ -0,0 +1,16 @@
+package env
+
+import "testing"
+
+func TestTask018DefaultFillsZero(t *testing.T) {
+	v := struct {
+		Port int `env:"PORT" envDefault:"8080"`
+	}{}
+	e := ParseWithOptions(&v, Options{Environment: map[string]string{}, SetDefaultsForZeroValuesOnly: true})
+	if e != nil {
+		t.Fatal(e)
+	}
+	if v.Port != 8080 {
+		t.Fatalf("Port=%d", v.Port)
+	}
+}
